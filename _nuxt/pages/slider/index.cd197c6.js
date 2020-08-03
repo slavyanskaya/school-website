a7552364f638f8f378ce2efcd88c28fbc036f72e
Hayk Karapetyan <haykokalipsis@gmail.com>
2020-08-03 22:14:36 +0400

    Slider fixes static

---
+++ _nuxt/pages/slider/index.cd197c6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{231:function(n,o,l){"use strict";l.r(o);var e=l(6),component=Object(e.a)({},void 0,void 0,!1,null,null,null);o.default=component.exports}}]);
